f2937df0f4c61bf1c11ce28c088a23a3c8d90afd
Emanuelle-02 <85840803+Emanuelle-02@users.noreply.github.com>
2023-02-11 13:05:52 -0300

    Selenium

--- MyClinic/apps/accounts/tests/functional_test/test_functional_pages.py
+++ MyClinic/apps/accounts/tests/functional_test/test_functional_pages.py
@@ -1,20 +1,29 @@
-from django.test import LiveServerTestCase
+import os
+import time
+from time import sleep
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import LiveServerTestCase, TestCase
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
 
-# Create your tests here.
 
-class FunctionalTest(LiveServerTestCase):
-    driver = webdriver.Chrome()
+class FunctionalTest(TestCase, LiveServerTestCase):
     def test_main_navegation_register_page_text(self):
-        
-        self.browser.get('http://127.0.0.1:8000/')
-        
-        assert "MyClinic - Página inicial" in self.browser.title
-        
-        about_link = self.browser.find_element(By.LINK_TEXT, 'op_login')
-        about_link.click()
-        
-        assert "MyClinic" in self.browser.title
-        
-        pass
+        browser = webdriver.Chrome()
+        browser.get("http://127.0.0.1:8000/admin_login/")
+        wait = WebDriverWait(browser, 5)
+
+        username = browser.find_element(By.NAME,"username")
+        username.clear()
+        username.send_keys("Administrador")
+        password = browser.find_element(By.NAME, "password")
+        password.send_keys("clinica123")
+        password.send_keys(Keys.RETURN)
+        sleep(1)
+        browser.quit()
