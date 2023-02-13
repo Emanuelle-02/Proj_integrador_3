15112bc8552f67482d4c11e70373c97370dfde3f
Emanuelle-02 <85840803+Emanuelle-02@users.noreply.github.com>
2023-02-13 01:03:37 -0300

    Correção

--- MyClinic/apps/administrador/tests/functional_test/test_functional_pages.py
+++ MyClinic/apps/administrador/tests/functional_test/test_functional_pages.py
@@ -1,4 +1,20 @@
-def test_admin_login_page(self):
+import os
+import time
+from time import sleep
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import LiveServerTestCase, TestCase
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+
+
+class FunctionalTest(TestCase, LiveServerTestCase):
+    def test_admin_login_page(self):
         browser = webdriver.Chrome()
         browser.get("http://127.0.0.1:8000/admin_login/")
         wait = WebDriverWait(browser, 5)
